d9aad247a5ed171797987366061992876156acae
Zalci004 <zk81508@student.uni-lj.si>
2024-03-02 16:54:07 +0100

    Dodan izpis #3

--- v1/Gravitacija.java
+++ v1/Gravitacija.java
@@ -2,4 +2,9 @@ public class Gravitacija {
 	public static void main(String[] arg) {
 		System.out.println("OIS je zakon!");
 	}
+	public static void izpis(double visina){
+		System.out.println(visina);
+		
+		System.out.println(izracunGravitacije(visina));
+	}
 }
