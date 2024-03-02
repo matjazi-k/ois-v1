64c573541b55d7b1320484d8a573067d9c84373e
matjazi-k <mk92169@student.uni-lj.si>
2024-03-02 16:15:13 +0100

    Dodaj izračun pospeška #3

--- v1/Gravitacija.java
+++ v1/Gravitacija.java
@@ -2,4 +2,11 @@ public class Gravitacija {
 	public static void main(String[] arg) {
 		System.out.println("OIS je zakon!");
 	}
+	
+	public static double izracunGravitecije(double visina) {
+		double C = 6.67e-11;
+		double M = 5.972e24;
+		double r = 6.371e6;
+		return (C * M) / Math.pow(r + visina, 2.0)
+	}
 }
